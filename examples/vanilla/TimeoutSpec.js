e7a137a38236d7a749b53f73450b24e288cc40d4
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-03-10 22:56:14 +0200

    added an example on how to test timeouts

---
+++ examples/vanilla/TimeoutSpec.js
@@ -0,0 +1,19 @@
+describe('timeout', function() {
+
+    // This test demonstrates how to test timeouts. Intervals are tested in the same manner.
+    // Note that the clock should be installed before the test and uninstalled after.
+    it('should call a function after some time', function() {
+        jasmine.clock().install();
+
+        var spy = jasmine.createSpy();
+        setTimeout(function() {
+            spy();
+        }, 200);
+
+        expect(spy).not.toHaveBeenCalled();
+        jasmine.clock().tick(200);
+        expect(spy).toHaveBeenCalled();
+
+        jasmine.clock().uninstall();
+    });
+});
