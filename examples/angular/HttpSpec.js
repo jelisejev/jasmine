2d18b3c98b3325f32db635e2bcc898c50bf5d05e
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-03-18 22:30:24 +0200

    added a http service example

---
+++ examples/angular/HttpSpec.js
@@ -0,0 +1,22 @@
+describe('Http', function() {
+    var $http, $httpBackend;
+
+    beforeEach(inject(function(_$http_, _$httpBackend_) {
+        $http = _$http_;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    it('should trigger a callback when the request is complete', function() {
+        $httpBackend.whenGET('http://google.com').respond(200, '');
+
+        var complete = false;
+        $http.get('http://google.com').success(function() {
+            complete = true;
+        });
+
+        // flush() returns the response
+        $httpBackend.flush();
+
+        expect(complete).toEqual(true);
+    });
+});
